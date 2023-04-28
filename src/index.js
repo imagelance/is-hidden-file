669ebb01d83c420901b16c8615abf0c571e18723
Patrik Jánosdeák  <patrik.janosdeak@dentsu.com>
2023-04-28 11:56:49 +0200

    feature: make hidden file detection work with windows paths

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const path = require('path');
+const os = require('os');
+
 // match any path that includes hidden file/directory starting with .
 // https://regex101.com/r/xxtBUN/1
-const hiddenPathRegex = /\/\..+/g;
+const unixHiddenPathRegex = /\/\..+/g;
+// https://regex101.com/r/zhOcF2/1
+const windowsHiddenPathRegex = /\\\..+/g;
 
-function normalizeValue(value) {
-	if (value[0] === path.sep) {
-		return value;
-	}
-
-	return `${ path.sep }${ value }`;
+const isWindows = function detectingIfPlatformIsWindows() {
+	return os.platform() === 'win32';
 }
 
-module.exports = function isHiddenFile(value) {
+const isHiddenFile = function detectingIfPathIsHiddenFile(value) {
 	if (typeof value !== 'string') {
 		throw new Error('Supplied value must be a string.');
 	}
 
-	return normalizeValue(value).match(hiddenPathRegex) !== null;
+	const pattern = isWindows() ? windowsHiddenPathRegex : unixHiddenPathRegex;
+	const normalizedValue = path.join(value);
+
+	return normalizedValue.match(pattern) !== null;
 };
+
+module.exports = isHiddenFile;
--- tests/isHiddenFile.test.js
+++ tests/isHiddenFile.test.js
@@ -1,4 +1,5 @@
 const isHiddenFile = require('../src/index');
+const mockOs = require('mock-os');
 
 test('it should be a hidden file', () => {
 	expect(isHiddenFile('/.DS_Store')).toBe(true);
@@ -11,6 +12,22 @@ test('it should not be a hidden file', () => {
 test('it should normalize file path and not be a hidden file', () => {
 	expect(isHiddenFile('cic/mic/zic.js')).toBe(false);
 });
+
+test('it should properly detect hidden file on windows', () => {
+	mockOs({ 'platform': 'win32' });
+
+	expect(isHiddenFile('C:\\Users\\Admin\\Projects\\test\\.git')).toBe(true);
+
+	mockOs.restore();
+});
+
+test('it should not detect unix paths as hidden paths on windows', () => {
+	mockOs({ 'platform': 'win32' });
+
+	expect(isHiddenFile('/Users/Admin/Projects/test/.DS_Store')).toBe(false);
+
+	mockOs.restore();
+})
 
 test('it should throw an error, if supplied argument is not a string', () => {
 	expect(() => isHiddenFile({})).toThrowError(new Error('Supplied value must be a string.'));
